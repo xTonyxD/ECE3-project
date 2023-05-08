d14b244fed427f1ef123a7f36817f5cf4d6fdd52
Lana Lim <lanachloe@g.ucla.edu>
2023-05-08 12:58:36 -0700

    normalizeSensorValues()

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,7 @@
 #include <ECE3.h>
+#define NUM_SENS 8
 
-uint16_t sensorValues[8];
+uint16_t sensorValues[NUM_SENS];
 
 void setup()
 {
@@ -14,14 +15,28 @@ void readIRsensors() {
 }
 
 void normalizeSensorValues() {
+  // subtract min, divide by max, multiply by 1000
+
+  // find min and max element
+  int i;
+  uint16_t min = 0;
+  uint16_t max = 0;
+  for(i=0;i<NUM_SENS;i++) {
+    if(sensorValues[i] <= min)
+      min = sensorValues[i];
+    if(sensorValues[i] >= max)
+      max = sensorValues[i];
+  }
+  // normalize sensor values
+  for(i=0;i<NUM_SENS;i++) 
+    sensorValues[i] = (sensorValues[i] - min)/max * 1000;
+}
+
+void weightSensorValues() {
 
 }
 
-void weightSensorValues {
-
-}
-
-void calculateMotorOutputs{
+void calculateMotorOutputs() {
   
 }
 
@@ -32,7 +47,7 @@ void loop()
 
   // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
   // 2500 means minimum reflectance
-  for (unsigned char i = 0; i < 8; i++)
+  for (unsigned char i = 0; i < NUM_SENS; i++)
   {
     Serial.print(sensorValues[i]);
     Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor

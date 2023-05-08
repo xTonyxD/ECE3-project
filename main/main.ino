6003f949992ba9a28e4109ce93f3ee960c9cf0c6
Lana Lim <lanachloe@g.ucla.edu>
2023-05-08 13:08:51 -0700

    Beep boop bap

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,8 @@
 #include <ECE3.h>
 #include <constants.h>
-
-uint16_t sensorValues[8] = [0, 0, 0, 0, 0, 0, 0, 0];
 #define NUM_SENS 8
+
+uint16_t sensorValues[NUM_SENS] = [0, 0, 0, 0, 0, 0, 0, 0];
 
 uint16_t sensorValues[NUM_SENS];
 
@@ -11,11 +11,6 @@ void setup()
   ECE3_Init();
   Serial.begin(9600); // set the data rate in bits per second for serial data transmission
   delay(2000);
-}
-
-// Reads IR sensors. outputs 
-uint_16 readIRsensors() {
-
 }
 
 void normalizeSensorValues() {
@@ -50,9 +45,14 @@ void loop()
 {
   // read raw sensor values
   ECE3_read_IR(sensorValues);
+  normalizeSensorValues();
+  weightSensorValues();
+  calculateMotorOutputs();
 
+  // PREVIOUS CODE START //
   // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
   // 2500 means minimum reflectance
+  /*
   for (unsigned char i = 0; i < NUM_SENS; i++)
   {
     Serial.print(sensorValues[i]);
@@ -61,4 +61,6 @@ void loop()
   Serial.println();
 
   delay(50);
+  */
+  // PREVIOUS CODE END //
 }
